d37ca1bea2531fede29eff0150bf3102cf9dbed7
zcalhoun <zcalhoun.ny@gmail.com>
2022-02-07 20:26:30 -0500

    Made a fix to the code to make this more clear.

--- scripts/twitter/main.py
+++ scripts/twitter/main.py
@@ -8,22 +8,32 @@ months = ['01','02','03','04','05','06','07','08','09','10','11','12']
 years = ['2018','2019']#,'2020','2021']
 all_months = [y+'-'+m for y in years for m in months]
 
+# Last month
+# The script stops when it hits this month, and does not get data from this month.
+last_month = "2019-01"
+
 # Construct query term
-words = [
-    "breathe",
-    "asthma",
-    "lungs",
-    "cough",
-    "headache",
-    "(itchy eyes)",
-    "(sore throat)"]
+# words = [
+#     "breathe",
+#     "asthma",
+#     "lungs",
+#     "cough",
+#     "headache",
+#     "(itchy eyes)",
+#     "(sore throat)"]
 # EDIT here to change the query...
 #query_term = "("+" OR ".join(words)+") -has:links -is:retweet has:geo -has:media place_country:us"
 
-# In this case, we do not want to query on ANY words.
+# In some cases, we do not want to query on ANY words.
+# In this case, we want to pull tweets directly for San Francisco
 query_term = "-has:links -is:retweet point_radius:[-122.5 37.8 25mi] has:geo lang:en -has:media place_country:us" 
 
+# EDIT here for 
+data_target_dir = '../../data/san_francisco/'
+
+# You should not need to edit anything below here.
 errors = []
+
 for i in range(len(all_months)):
     
     print("Starting query for", all_months[i])
@@ -45,11 +55,11 @@ for i in range(len(all_months)):
         continue
     # Save tweets to a file.
     # EDIT here to change file destination
-    tweets.to_csv('../../data/san_francisco/'+all_months[i]+'.csv', index=False)
+    tweets.to_csv(data_target_dir+all_months[i]+'.csv', index=False)
     # Break if last month is this month
     print()
     # EDIT here to be the last month
-    if (all_months[i+1] == "2019-01"):
+    if (all_months[i+1] == last_month):
         print("Script complete")
         break
     else:
@@ -59,6 +69,6 @@ for i in range(len(all_months)):
 
 if(len(errors) > 0):
     # EDIT here to change file destination
-    with open('../../data/san_francisco/errors.txt', 'w') as f:
+    with open(data_target_dir+'errors.txt', 'w') as f:
         for e in errors:
             f.write("%s\n" % e)
