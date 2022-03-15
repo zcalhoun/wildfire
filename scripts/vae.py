24434f33e4a2087cc64d1424bd237f7fdff7bd3b
zcalhoun <zcalhoun.ny@gmail.com>
2022-03-15 10:12:07 -0400

    Checkpoint -- added data handler

--- scripts/vae.py
+++ scripts/vae.py
@@ -37,7 +37,7 @@ class TweetData():
 
 		# Define
 		self.test_size = 0.2
-		self.count_vectorizer = CountVectorizer(min_df=min_df, max_df=max_df)
+		self.count_vectorizer = CountVectorizer(stop_words='english',min_df=min_df, max_df=max_df)
 
 	def get_tweet_count_vecs(self):
 		# Load the data
@@ -46,22 +46,11 @@ class TweetData():
 
 		# If debug, only keep the first 100 tweets
 		if(self.debug):
-			data = data.iloc[0:100]
+			data = data.iloc[0:1000]
 		
-		# Clean the text
-		tt = TweetTokenizer()
-		nlp = spacy.load('en_core_web_sm', disable=['parser','ner'])
-		lemmatized = []
-		for tweet in data['text']:
-			t = tweet.lower()
-			t = tt.tokenize(t)
-			t = [w for w in t if w.isalpha()]
-			doc = nlp(" ".join(t))
-			lemmatized.append(" ".join([token.lemma_ for token in doc]))
-		
-		data['lemmatized'] = lemmatized
-		# Split into train/test splits
-		X_train, X_test = train_test_split(lemmatized, test_size=self.test_size, random_state=42)
+		data = self.preprocess(data)
+
+		X_train, X_test = train_test_split(data, test_size=self.test_size, random_state=42)
 		
 		# Apply count vectorizer
 		self.X_train = self.count_vectorizer.fit_transform(X_train)
@@ -70,9 +59,32 @@ class TweetData():
 		# Return the train/test count vectors
 		return self.X_train, self.X_test
 
+	def preprocess(self, data):
+		tt = TweetTokenizer()
+		nlp = spacy.load('en_core_web_sm', disable=['parser','ner'])
+		lemmatized = []
+		for tweet in data['text']:
+			t = tweet.lower()
+			t = tt.tokenize(t)
+			# Removes hashtags and @s
+			t = [w for w in t if w.isalpha()]
+			doc = nlp(" ".join(t))
+			lemmatized.append(" ".join([token.lemma_ for token in doc]))
+
+		return lemmatized
+
+	def to_tensor_dataset(self):
+		x_train = TensorDataset(torch.from_numpy(self.X_train.todense()).float())
+		x_test = torch.from_numpy(self.X_test.todense()).float()
+		return x_train, x_test
+	
 	def get_count_vec(self):
 		return self.count_vectorizer
 
+	def inverse_transform_count_vec(self, count_vec):
+		# TODO - make this handle the CSR data format.
+		# if(type(count_vec) == torch.utils.data.dataset.TensorDataset):
+		return self.count_vectorizer.inverse_transform(count_vec)
 
 class TweetDataLoader():
 	def __init__(self, test=False):
@@ -97,7 +109,12 @@ class TweetDataLoader():
 # Handle any arguments passed at runtime.
 class VAE(nn.Module):
 
-	def __init__(self):
+	def __init__(self, vocab):
+		"""
+		Inputs
+		--------
+		vocab<int>: the size of the vocabulary
+		"""
 		super(VAE, self).__init__()
 
 		# VAE weights
@@ -107,8 +124,6 @@ class VAE(nn.Module):
 		self.fc3 = nn.Linear(20,400)
 		self.fc4 = nn.Linear(400, VOCAB)
 
-		# Prediction weights
-		self.fc_aqi = nn.Linear(20, 1)
 	def encode(self, x):
 		h1 = F.relu(self.fc1(x))
 		return self.fc21(h1), self.fc22(h1)
@@ -122,24 +137,19 @@ class VAE(nn.Module):
 		h3 = F.relu(self.fc3(z))
 		return torch.sigmoid(self.fc4(h3))
 
-	def predict_aqi(self, mu):
-		val = self.fc5(mu)
-		return val
-
 	def forward(self, x):
 		mu, logvar = self.encode(x.view(-1, 784))
 		z = self.reparameterize(mu, logvar)
-		aqi = self.predict_aqi(mu)
-		return self.decode(z), aqi, mu, logvar
+		return self.decode(z), mu, logvar
 
-	@torch.no_grad()
-	def predict(self, x):
-		'''
-		Handles predicting AQI for any vector of tweets.
-		'''
-		x = torch.Tensor(x).to(device)
-		a, aqi, b, c = self.forward(x)
-		return aqi
+	# @torch.no_grad()
+	# def predict(self, x):
+	# 	'''
+	# 	Handles predicting AQI for any vector of tweets.
+	# 	'''
+	# 	x = torch.Tensor(x).to(device)
+	# 	a, aqi, b, c = self.forward(x)
+	# 	return aqi
 
 
 # model = VAE().to(device)
@@ -152,9 +162,9 @@ def loss_function(recon_x, x, pred_aqi, aqi, mu, logvar):
 
 	KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-	MSE = torch.mean((pred_aqi-aqi).pow(2))
+	# MSE = torch.mean((pred_aqi-aqi).pow(2))
 
-	return BCE + KLD + MSE
+	return BCE + KLD
 
 def train(epoch):
 	model.train()
@@ -176,12 +186,21 @@ def train(epoch):
 	print('====> Epoch: {} Average loss: {:.4f}'.format(
 		epoch, train_loss / len(train_loader.dataset)))
 
+if __name__ == "__main__":
+	print("Begin testing")
+	
+	# Load data
+	v = TweetData(max_df=0.1, debug=True)
+	v.get_tweet_count_vecs()
+	x_tr_tensor, x_test_tensor = v.to_tensor()
+
+	train_loader = DataLoader(x_tr_tensor, batch_size=10, shuffle=True)
+	test_loader = DataLoader(X_test_tensor, batch_size=10, shuffle=False)
+    
+    # Initialize model
+	
+	# Train model
+
+	# Test model
 
 if __name__ == "__main__":
-    # Running testing to validate model.
-    print("Testing beginning")
-
-    # Load training data
-    # Custom data loader required.
-
-    # Initialize model
