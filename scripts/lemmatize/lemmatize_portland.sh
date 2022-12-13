d77da8819731f7a0d2d034738eeb12b906ea2bbd
Zach Calhoun <zdc6@egr-zdc6-mbp.lan>
2022-12-12 21:41:11 -0500

    Update to portland script.

--- scripts/lemmatize/lemmatize_portland.sh
+++ scripts/lemmatize/lemmatize_portland.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --array 0-500
 
-singularity exec -B /hpc/group/carlsonlab/zdc6/ ../wildfire-tweets.sif python lemmatize_array.py \
+singularity exec -B /hpc/group/carlsonlab/zdc6/ ../../wildfire-tweets.sif python lemmatize_array.py \
 	-id /hpc/group/carlsonlab/zdc6/wildfire/data/tweets/portland/raw/ \
 	-td /hpc/group/carlsonlab/zdc6/wildfire/data/lemmatized/ \
 	-ad /hpc/group/carlsonlab/zdc6/wildfire/data/aqi/ \
